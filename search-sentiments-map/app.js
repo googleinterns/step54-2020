639d8b63b5a4a3f8c41c82bd896aa9204a521e33
ChenyuHeidiZhang <chenyu.heidi.z@gmail.com>
2020-06-26 15:57:09 -0400

    fix const names and style

--- search-sentiments-map/app.js
+++ search-sentiments-map/app.js
@@ -1,41 +1,40 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
 
 const PORT = process.env.PORT || 3000;
-const Http = require('http');
-const Fs = require('fs');
-const Path = require('path');
+const HTTP = require('http');
+const FS = require('fs');
+const PATH = require('path');
 
-// Create server that displays the webpage with the html, css, javascript, and png files in the public folder.
-Http.createServer(function(req, res) {
-  const publicPath = Path.join(__dirname, 'public', req.url);
+// Create server that displays the webpage with the html, css, javascript, and png files in the
+// public folder.
+HTTP.createServer(function(req, res) {
+  const publicPath = PATH.join(__dirname, 'public', req.url);
 
   if (req.url === "/") {
-    Fs.readFile("./public/index.html", "UTF-8", function(err, html) {
+    FS.readFile("./public/index.html", "UTF-8", function(err, html) {
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(html);
     });
   } else if (req.url.match("\.css$")) {
-    var fileStream = Fs.createReadStream(publicPath, "UTF-8");
+    var fileStream = FS.createReadStream(publicPath, "UTF-8");
     res.writeHead(200, {"Content-Type": "text/css"});
     fileStream.pipe(res);
   } else if (req.url.match("\.js$")) {
-    var fileStream = Fs.createReadStream(publicPath, "UTF-8");
+    var fileStream = FS.createReadStream(publicPath, "UTF-8");
     res.writeHead(200, {"Content-Type": "text/javascript"});
     fileStream.pipe(res);
   } else if (req.url.match("\.png$")) {
-    var fileStream = Fs.createReadStream(publicPath);
+    var fileStream = FS.createReadStream(publicPath);
     res.writeHead(200, {"Content-Type": "image/png"});
     fileStream.pipe(res);
   } else {
