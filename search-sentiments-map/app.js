c3daf074c0e1ea37541c7e2e69b6ef2947baa2e5
ChenyuHeidiZhang <chenyu.heidi.z@gmail.com>
2020-06-26 17:31:38 -0400

    fix line wrapping

--- search-sentiments-map/app.js
+++ search-sentiments-map/app.js
@@ -1,13 +1,15 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License
-// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-// or implied. See the License for the specific language governing permissions and limitations under
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
 // the License.
 
 const PORT = process.env.PORT || 3000;
@@ -15,8 +17,8 @@ const HTTP = require('http');
 const FS = require('fs');
 const PATH = require('path');
 
-// Create server that displays the webpage with the html, css, javascript, and png files in the
-// public folder.
+// Create server that displays the webpage with the html, css, javascript, and
+// png files in the public folder.
 HTTP.createServer(function(req, res) {
   const publicPath = PATH.join(__dirname, 'public', req.url);
 
