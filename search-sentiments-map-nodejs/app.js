cb26ef512f0dfcc24627237573e4132027fc0d99
ChenyuHeidiZhang <chenyu.heidi.z@gmail.com>
2020-06-26 13:55:19 -0400

    made new nodejs app

---
+++ search-sentiments-map-nodejs/app.js
@@ -0,0 +1,59 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const PORT = process.env.PORT || 3000;
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(req, res) {
+  if (req.url === "/") {
+    fs.readFile("./public/index.html", "UTF-8", function(err, html) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(html);
+    });
+  } else if (req.url.match("\.css$")) {
+    var cssPath = path.join(__dirname, 'public', req.url);
+    var fileStream = fs.createReadStream(cssPath, "UTF-8");
+    res.writeHead(200, {"Content-Type": "text/css"});
+    fileStream.pipe(res);
+  } else if (req.url.match("\.js$")) {
+    var jsPath = path.join(__dirname, 'public', req.url);
+    var fileStream = fs.createReadStream(jsPath, "UTF-8");
+    res.writeHead(200, {"Content-Type": "text/javascript"});
+    fileStream.pipe(res);
+  } else if (req.url.match("\.png$")) {
+    var imagePath = path.join(__dirname, 'public', req.url);
+    var fileStream = fs.createReadStream(imagePath);
+    res.writeHead(200, {"Content-Type": "image/png"});
+    fileStream.pipe(res);
+  } else {
+    res.writeHead(404, {"Content-Type": "text/html"});
+    res.end("No Page Found");
+  }
+}).listen(PORT);
+
+const googleTrends = require('google-trends-api');
+
+googleTrends.interestOverTime({keyword: 'Women\'s march'})
+.then(function(results) {
+  console.log('These results are awesome', results);
+})
+.catch(function(err) {
+  console.error('Oh no there was an error', err);
+});
+
+function setTopTrends() {
+  
+}
