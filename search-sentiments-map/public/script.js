60404c94ce2afb9bdff10e11261df7f60e83992f
ChenyuHeidiZhang <chenyu.heidi.z@gmail.com>
2020-06-29 20:48:11 -0400

    rename constants

--- search-sentiments-map/public/script.js
+++ search-sentiments-map/public/script.js
@@ -19,12 +19,12 @@ function createWorldMap() {
     {center: {lat: 38.46049, lng: -5.428423}, zoom: 3});
 }
 
-const SEE_MORE_OR_LESS_ID = 'see-more-or-less';
-const SEE_MORE = 'See More';
-const SEE_LESS = 'See Less';
+const SHOW_MORE_OR_LESS_ID = 'show-more-or-less';
+const TOGGLE_SHOW_MORE = 'Show More';
+const TOGGLE_SHOW_LESS = 'Show Less';
 
-const SHOWN = 'shown';
-const HIDDEN = 'hidden';
+const CLASSNAME_SHOWN = 'shown';
+const CLASSNAME_HIDDEN = 'hidden';
 
 /** Displays the top trends of the US on the DOM. */
 function setTopTrends() {
@@ -38,40 +38,40 @@ function setTopTrends() {
       var trendElement = document.createElement('li');
       trendElement.innerText = trendingSearches[i].title.query;
       // Show 10 trending topics by default and hide the rest.
-      trendElement.className = i < 10 ? SHOWN : HIDDEN;
+      trendElement.className = i < 10 ? CLASSNAME_SHOWN : CLASSNAME_HIDDEN;
       trendsList.append(trendElement);
     }
 
     // Add an item to the list that toggles showing more or less topics when there are more
     // than 10 trending topics.
     if (trendingSearches.length > 10) {
-      const seeMoreOrLessToggleItem = document.createElement('li');
-      seeMoreOrLessToggleItem.innerText = SEE_MORE;
-      seeMoreOrLessToggleItem.id = SEE_MORE_OR_LESS_ID;
-      seeMoreOrLessToggleItem.addEventListener('click', () => {
+      const showMoreOrLessToggleItem = document.createElement('li');
+      showMoreOrLessToggleItem.innerText = TOGGLE_SHOW_MORE;
+      showMoreOrLessToggleItem.id = SHOW_MORE_OR_LESS_ID;
+      showMoreOrLessToggleItem.addEventListener('click', () => {
         showMoreOrLess();
       });
-      trendsList.append(seeMoreOrLessToggleItem);
+      trendsList.append(showMoreOrLessToggleItem);
     }
   });
 }
 
-/** 
+/**
  * Shows the remaining 10 trending topics if the user clicks on 'See More' and
  * hides the last 10 if the user clicks on 'See Less.'
  */
 function showMoreOrLess() {
-  const seeMoreOrLessToggleItem = document.getElementById(SEE_MORE_OR_LESS_ID);
+  const showMoreOrLessToggleItem = document.getElementById(SHOW_MORE_OR_LESS_ID);
   const trendElements = document.querySelectorAll('#trends-list li');
-  if (seeMoreOrLessToggleItem.innerText === SEE_MORE) {
+  if (showMoreOrLessToggleItem.innerText === TOGGLE_SHOW_MORE) {
     for (var i = 10; i < trendElements.length - 1; i++){
-      trendElements[i].className  = SHOWN;
+      trendElements[i].className  = CLASSNAME_SHOWN;
     }
-    seeMoreOrLessToggleItem.innerText = SEE_LESS;
+    showMoreOrLessToggleItem.innerText = TOGGLE_SHOW_LESS;
   } else {
     for (var i = 10; i < trendElements.length - 1; i++) {
-      trendElements[i].className  = HIDDEN;
+      trendElements[i].className  = CLASSNAME_HIDDEN;
     }
-    seeMoreOrLessToggleItem.innerText = SEE_MORE;
+    showMoreOrLessToggleItem.innerText = TOGGLE_SHOW_MORE;
   }
 }
