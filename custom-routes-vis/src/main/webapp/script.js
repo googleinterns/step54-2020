1537b31da659fe7a64e2368becee2c383bd66156
Carmen Benitez <carlorben@gmail.com>
2020-07-08 16:18:34 +0000

    Added ability to select origin and destination locations and move them and display them

--- custom-routes-vis/src/main/webapp/script.js
+++ custom-routes-vis/src/main/webapp/script.js
@@ -17,4 +17,43 @@ function createWorldMap() {
   map = new google.maps.Map(
     document.getElementById('map'),
     {center: {lat: 38.46049, lng: -5.428423}, zoom: 3});
+  
+  let clickCount = 0;
+  let placeOriginListener = google.maps.event.addListener(map, 'click', function (event) {
+    if (clickCount == 0) {
+      updateCoordinates(event.latLng.lat(), event.latLng.lng(), 'origin-coordinates');
+
+      let originMarker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        draggable: true,
+        label: 'Origin',
+      });
+
+      originMarker.addListener('dragend', function(event){
+        updateCoordinates(event.latLng.lat(), event.latLng.lng(), 'origin-coordinates');
+      });
+      clickCount++;
+    } else if (clickCount == 1) {
+      updateCoordinates(event.latLng.lat(), event.latLng.lng(), 'destination-coordinates');
+
+      let destinationMarker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        draggable: true,
+        label: 'Destination',
+      });
+
+      destinationMarker.addListener('dragend', function(event){
+        updateCoordinates(event.latLng.lat(), event.latLng.lng(), 'destination-coordinates');
+      });
+      clickCount++;
+`   } else {`
+      google.maps.event.removeListener(placeOriginListener);
+    }
+  });
+}
+
+function updateCoordinates(lat, lng, containerId) {
+  document.getElementById(containerId).innerHTML =  "Latitude: " + lat + "<br>Longitude: " + lng;
 }
