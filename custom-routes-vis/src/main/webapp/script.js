513bf9851601f5a64002e4f19cd41fe2bd0f0444
Carmen Benitez <carlorben@gmail.com>
2020-07-08 17:42:59 -0400

    Added ability to delete origin/destination markers and move them.

--- custom-routes-vis/src/main/webapp/script.js
+++ custom-routes-vis/src/main/webapp/script.js
@@ -12,44 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Array holding origin and destination markers.
+let markers = []
+
 /** Creates the world map and markers. */
 function createWorldMap() {
   map = new google.maps.Map(
     document.getElementById('map'),
     {center: {lat: 38.46049, lng: -5.428423}, zoom: 3});
   
-  // First click creates origin marker, second click creates destination
-  // marker. Removes listener after first two clicks.
-  let clickCount = 0;
-  let placeOriginListener = google.maps.event.addListener(map, 'click', 
-      function (event) {
-    if (clickCount == 0) {
-      createMarker('origin-coordinates', 'Origin', event.latLng);
-      clickCount++;
-    } else if (clickCount == 1) {
-      createMarker('destination-coordinates', 'Destination', event.latLng);
-      clickCount++;
-`   } else {`
-      google.maps.event.removeListener(placeOriginListener);
-    }
-  });
+  // Create origin and destination markers, but hides them until user selects
+  // coordinates.
+  createMarker('origin-coordinates', 'A', 'Origin',  {lat: 0.0, lng: 0.0});
+  createMarker('destination-coordinates', 'B', 'Destination',
+      {lat: 0.0, lng: 0.0});
 }
 
 /**
- * Creates marker that updates coordiantes on page when moved.
+ * Creates hidden marker that updates coordiantes on page when moved.
  * @param {string} containerId id of container to update coordinates in.
  * @param {string} label Label to display on container.
- * @param {Object} latLng Coordinates of where user clicked.
+ * @param {Object} latLng Default coordinates of where to place marker.
  */
-function createMarker (containerId, label, latLng) {
-  updateCoordinates(latLng.lat(), latLng.lng(), containerId);
-
+function createMarker (containerId, label, title, latLng) {
   let marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
     draggable: true,
     label: label,
+    map: map,
+    position: latLng,
+    title: title,
+    visible: false,
   });
+  // Add marker to markers array to access in other functions.
+  markers.push(marker);
 
   marker.addListener('dragend', function(event){
     updateCoordinates(event.latLng.lat(), event.latLng.lng(), containerId);
@@ -63,4 +58,40 @@ function createMarker (containerId, label, latLng) {
 function updateCoordinates(lat, lng, containerId) {
   document.getElementById(containerId).innerHTML = 
       "Latitude: " + lat + "<br>Longitude: " + lng + "<br>";
+}
+
+/**
+ * Hides 'place marker' button and waits for user to select coordinate for
+ * marker. Updates marker to be in correct location, and show 'delete marker'
+ * button.
+ * @param {int} markerIndex Index of corresponding marker in markers array.
+ */
+function showMarker(markerIndex) {
+  let containerName = ((markerIndex === 0) ? 'origin' : 'destination');
+  let placeMarkerListener = google.maps.event.addListener(map, 'click', 
+      function (event) {
+    markers[markerIndex].setPosition(event.latLng);
+    google.maps.event.removeListener(placeMarkerListener);
+    markers[markerIndex].setVisible(true);
+    updateCoordinates(event.latLng.lat(), event.latLng.lng(), containerName + '-coordinates');
+    document.getElementById('hide-' + containerName + '-marker').style.display = 'block';
+  });
+
+  let containerToHide = 'show-' + ((markerIndex === 0) ? 'origin' : 'destination') + '-marker'
+  document.getElementById(containerToHide).style.display = 'none';
+  document.getElementById(containerName + '-coordinates').innerHTML = 'Click the map to select location!';
+}
+
+/**
+ * Hides specified marker and toggles buttons to show 'place marker' button.
+ * @param {int} markerIndex Index of corresponding marker in markers array.
+ */
+function hideMarker(markerIndex) {
+  markers[markerIndex].setVisible(false);
+  let containerName = ((markerIndex === 0) ? 'origin' : 'destination');
+  document.getElementById(containerName + '-coordinates').innerHTML = '';
+  document.getElementById('show-' + containerName + '-marker').style.display =
+      'block';
+  document.getElementById('hide-' + containerName + '-marker').style.display =
+      'none';
 }
