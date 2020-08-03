8422f3e813bc952d82a893c8427da72420e485a4
Carmen Benitez <carlorben@gmail.com>
2020-08-03 09:08:32 -0400

    added license, removed empty line

--- search-sentiments-map/test/search-test.js
+++ search-sentiments-map/test/search-test.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 const chai = require('chai');
 const assert = chai.assert;
 const sinon = require('sinon');
@@ -211,7 +225,6 @@ describe('Search', function() {
       deleteAncientResultsStub = 
           sinon.stub(search, 'deleteAncientResults')
               .resolves('Not interested in the output');
-
 
       // Stub the `getSearchResultsForCountriesForTopic` function for this
       // test since it is tested separately.
